2a338523330eae2b7d9cbef663348abed8b9572e
guojijun <cq6@qq.com>
2026-07-16 19:00:43 +0800

    video_wan_dancer

---
+++ nodes/gjj_tian_align.py
@@ -0,0 +1,28 @@
+class GJJ_TianAlign:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "说明": ("STRING", {
+                    "default": "选择两个或更多节点后按 Alt+A 打开田字格对齐面板。",
+                    "multiline": True,
+                }),
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "execute"
+    CATEGORY = "GJJ/零依赖"
+    OUTPUT_NODE = False
+
+    def execute(self, 说明=""):
+        return ()
+
+
+NODE_CLASS_MAPPINGS = {
+    "GJJ_TianAlign": GJJ_TianAlign,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "GJJ_TianAlign": "田字格对齐 (Alt+A)",
+}
